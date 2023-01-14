4a4657728a9182d2a5a0d773756445561172a7e6
Soni Kumari <sonikumaribca999@gmail.com>
2023-01-14 15:50:44 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { createContext, useState } from 'react';
+import './App.css';
+import Counter from './Component/Counter';
+export const globalInfo = createContext();
+
+function App() {
+  const [count, setCount] = useState(0);
+  function update(){
+    setCount(count+1)
+  }
+
+  function decrease(){
+    setCount(count-1)
+  }
+  return (
+    <globalInfo.Provider  value={{Count: count , update:update, decrease:decrease}} >
+    <div className="App">
+      <Counter />
+    </div>
+    </globalInfo.Provider>
+  );
+}
+
+export default App;
