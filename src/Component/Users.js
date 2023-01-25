31d9411497ec9e1d9828e15664f09f2ac380fe40
Ankit kumar <ankitkumarasus@gmail.com>
2023-01-25 18:08:22 +0530

    initial changes

--- src/Component/Users.js
+++ src/Component/Users.js
@@ -2,12 +2,6 @@ import React, { useEffect } from 'react';
 import { getAllUsersData } from './api';
 
 const Users = () => {
-
-    useEffect(()=>{
-        getAllUsersData().than(res=>{
-            console.log(res,"data")
-        })
-    },[])
     return (
         <div>
 
