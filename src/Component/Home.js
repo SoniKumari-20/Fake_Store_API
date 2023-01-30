513685abffc13f4cbe36e5917c70533bd0b03a4d
Soni Kumari <sonikumaribca999@gmail.com>
2023-01-30 23:44:20 +0530

    filterrr

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,18 +1,18 @@
 import './style.css'
-import { Link, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useContext, useEffect, useState } from 'react';
 import { MainContext } from './content/MainProvider';
 import { getProductCategories } from './api';
-import { calculateNewValue } from '@testing-library/user-event/dist/utils';
 
 export default function Home() {
 
-  const {category, allProducts, setAllProducts} = useContext(MainContext)
-
+  const {category, allProducts} = useContext(MainContext)
+  
 const [filter, setFilter] = useState(allProducts)
-const [data, setData] = useState("")
 
-console.log(data)
+const [data, setData] = useState({})
+
+// console.log(data)
 
 const getCategoryData = async () => {
   getProductCategories(data).then((res) =>
@@ -22,11 +22,8 @@ const getCategoryData = async () => {
 
 useEffect(() => {
   getCategoryData()
-}, [])
+}, [data])
 
-const  filterData = (cat) => {
-  setData(cat)
-}
 
 
   
@@ -42,13 +39,12 @@ const  filterData = (cat) => {
             <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
               All Category
             </button>
-            <ul class="dropdown-menu">
-
+            <ul class="dropdown-menu"> 
             <li class="btn btn-light  "  style={{height: 40, width: 250,  margin:2}} onClick={ () => setFilter(allProducts)} >ALL</li>
-              <li class="btn btn-light  "  style={{height: 40, width: 250,  margin:2}} onClick={ () => filterData("electronics")} >{category[0]} </li>
-              <li class="btn btn-light"  style={{height: 40,width: 250,  margin:2}} onClick={() =>filterData("jewelery")}>{category[1]}</li>
-              <li class="btn btn-light"  style={{height: 40,width: 250, margin:2}} onClick={() =>filterData("men's clothing")} >{category[2]}</li>
-              <li class="btn btn-light"  style={{height: 40,width: 250 , margin:2}} onClick={() =>filterData("women's clothing")}>{category[3]}</li>
+              <li class="btn btn-light  "  style={{height: 40, width: 250,  margin:2}} onClick={ () => setData("electronics")} >{category[0]} </li>
+              <li class="btn btn-light"  style={{height: 40,width: 250,  margin:2}} onClick={() =>setData("jewelery")}>{category[1]}</li>
+              <li class="btn btn-light"  style={{height: 40,width: 250, margin:2}} onClick={() =>setData("men's clothing")} >{category[2]}</li>
+              <li class="btn btn-light"  style={{height: 40,width: 250 , margin:2}} onClick={() =>setData("women's clothing")}>{category[3]}</li>
             </ul>
           </div>
         </div>
@@ -59,8 +55,8 @@ const  filterData = (cat) => {
           <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 ">
             {
               filter.map((items, id) =>
-                <div class="col-sm my-4">
-                  <div className="card  cart_item" key={id} >
+                <div class="col-sm my-4" key={id}>
+                  <div className="card  cart_item" key={id+1} >
                     <img className="card-img-top" src={items.image} alt="Card image cap" height={"200px"} />
                     <div className="card-body text-center " height={"230px"} width={"230px"}>
                       <h5 className="card-title">{items.category}</h5>
